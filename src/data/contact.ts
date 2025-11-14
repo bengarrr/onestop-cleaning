d307a0efad6645d18dee472a78d4b5a78b2281cc
bengarrr <udlrsc@gmail.com>
2025-11-13 21:34:23 -0800

    contact form changes and image gallery updates

---
+++ src/data/contact.ts
@@ -0,0 +1,6 @@
+export const contact = {
+    phone: "(949) 307-6524",
+    phone_raw: "+1-949-307-6524",
+    email: "info@onestopcleaningsolution.com",
+    address: "Santa Ana, CA"
+}
---
+++ src/data/social.ts
@@ -0,0 +1,28 @@
+export const social = {
+    links: {
+        facebook: {
+            url: "https://www.facebook.com/carpetonestop/",
+            icon: "ri-facebook-fill"
+        },
+        linkedin: {
+            url: "",
+            icon: ""
+        },
+        twitter: {
+            url: "",
+            icon: ""
+        },
+        youtube: {
+            url: "",
+            icon: ""
+        },
+        instagram: {
+            url: "https://www.instagram.com/onestopcarpetcleaning",
+            icon: "ri-instagram-fill"
+        },
+        yelp: {
+            url: "https://www.yelp.com/biz/one-stop-carpet-cleaning-santa-ana-3",
+            icon: "fa-brands fa-yelp"
+        },
+    }
+}
